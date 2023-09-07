54fbb6c501aad42c99e8bad77e4beab4f56ae269
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-09-07 06:05:00 +0100

    App navigation setup

---
+++ app/src/main/java/com/shegs/profiledetails/navigation/AppNavigation.kt
@@ -0,0 +1,26 @@
+package com.shegs.profiledetails.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.shegs.profiledetails.ui.screens.GithubWebViewScreen
+import com.shegs.profiledetails.ui.screens.ProfileScreen
+
+@Composable
+fun AppNavigation() {
+
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "profileScreen"){
+        composable("profileScreen"){
+            ProfileScreen()
+        }
+
+        composable("githubScreen"){
+            GithubWebViewScreen()
+        }
+    }
+
+
+}
